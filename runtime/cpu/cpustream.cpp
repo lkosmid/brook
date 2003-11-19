eae33757a9b285e28fc60305e9a154d7245a7c6f
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2003-11-19 03:38:08 +0000

    *** empty log message ***

--- runtime/cpu/cpustream.cpp
+++ runtime/cpu/cpustream.cpp
@@ -1,29 +1,34 @@
+#pragma warning(disable:4786)
+#include "cpu.hpp"
+#ifdef _WIN32
+//the above warning disables visual studio's annoying habit of warning when using the standard set lib
+#endif
 #include <map>
 #include <string>
 #include <iostream>
-#include "cpu.hpp"
 using std::map;
 using std::string;
+typedef std::map<std::string,int> mapstrint;
 static map<string,int> getKnownTypes() {
     map<string,int>ret;
-    ret.insert(map<string,int>::value_type("float",sizeof(float)));
-    ret.insert(map<string,int>::value_type("float1",sizeof(float)));
-    ret.insert(map<string,int>::value_type("float2",sizeof(float2)));
-    ret.insert(map<string,int>::value_type("float3",sizeof(float3)));
-    ret.insert(map<string,int>::value_type("float4",sizeof(float4)));            
-    ret.insert(map<string,int>::value_type("int",sizeof(int)));
-    ret.insert(map<string,int>::value_type("int1",sizeof(int)));
-    ret.insert(map<string,int>::value_type("int2",2*sizeof(int)));
-    ret.insert(map<string,int>::value_type("int3",3*sizeof(int)));
-    ret.insert(map<string,int>::value_type("int4",4*sizeof(int)));            
-    ret.insert(map<string,int>::value_type("char",sizeof(char)));
-    ret.insert(map<string,int>::value_type("char1",sizeof(char)));
-    ret.insert(map<string,int>::value_type("char2",2*sizeof(char)));
-    ret.insert(map<string,int>::value_type("char3",3*sizeof(char)));
-    ret.insert(map<string,int>::value_type("char4",4*sizeof(char)));            
+    ret.insert(mapstrint::value_type("float",sizeof(float)));
+    ret.insert(mapstrint::value_type("float1",sizeof(float)));
+    ret.insert(mapstrint::value_type("float2",sizeof(float2)));
+    ret.insert(mapstrint::value_type("float3",sizeof(float3)));
+    ret.insert(mapstrint::value_type("float4",sizeof(float4)));            
+    ret.insert(mapstrint::value_type("int",sizeof(int)));
+    ret.insert(mapstrint::value_type("int1",sizeof(int)));
+    ret.insert(mapstrint::value_type("int2",2*sizeof(int)));
+    ret.insert(mapstrint::value_type("int3",3*sizeof(int)));
+    ret.insert(mapstrint::value_type("int4",4*sizeof(int)));            
+    ret.insert(mapstrint::value_type("char",sizeof(char)));
+    ret.insert(mapstrint::value_type("char1",sizeof(char)));
+    ret.insert(mapstrint::value_type("char2",2*sizeof(char)));
+    ret.insert(mapstrint::value_type("char3",3*sizeof(char)));
+    ret.insert(mapstrint::value_type("char4",4*sizeof(char)));            
     return ret;
 }
-static map<string,int> knownTypes = getKnownTypes();
+static mapstrint knownTypes = getKnownTypes();
 
 std::string noWhiteSpace (std::string in) {
 	unsigned int j=0;
