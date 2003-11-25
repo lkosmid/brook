677d0573ae270012ee132be33259cf2c774a8d09
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2003-11-25 06:45:40 +0000

    *** empty log message ***

---
+++ prog/apps/benchmark/lin_time.cpp
@@ -0,0 +1,134 @@
+/* 
+ * Vega Strike
+ * Copyright (C) 2001-2002 Daniel Horn
+ * 
+ * http://vegastrike.sourceforge.net/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ */
+
+static double firsttime;
+#ifdef WIN32
+#include <windows.h>
+static LONGLONG ttime;
+static LONGLONG newtime = 0;
+static LONGLONG freq;
+static double dblnewtime;
+#else
+#if defined( HAVE_SDL )
+#   include <SDL/SDL.h>
+#endif /* defined( HAVE_SDL ) */
+static double newtime;
+static double lasttime;
+
+#include <sys/time.h>
+#include <sys/types.h>
+#endif
+static double elapsedtime=.1;
+static double timecompression=1;
+double getNewTime() {
+#ifdef _WIN32
+	return dblnewtime-firsttime;
+#else
+	return newtime-firsttime;
+#endif
+}
+
+
+#ifdef _WIN32
+#include <windows.h>
+
+
+void micro_sleep(unsigned int n) {
+	Sleep(n / 1000);
+
+	return;
+}
+#elif defined(IRIX)
+#include <unistd.h>
+
+void micro_sleep(unsigned int n) {
+	(void) usleep((useconds_t)n);
+	return;
+}
+
+#else
+
+void micro_sleep(unsigned int n) {
+	struct timeval tv = { 0, 0 };
+
+	tv.tv_usec = n%1000000;
+	tv.tv_sec=n/1000000;
+	select(0, NULL, NULL, NULL, &tv);
+
+	return;
+}
+#endif
+
+void InitTime () {
+#ifdef WIN32
+  QueryPerformanceFrequency((LARGE_INTEGER*)&freq);
+  QueryPerformanceCounter((LARGE_INTEGER*)&ttime);
+
+#elif defined(HAVE_GETTIMEOFDAY)
+  struct timeval tv;
+  (void) gettimeofday(&tv, NULL);
+
+  newtime = (double)tv.tv_sec + (double)tv.tv_usec * 1.e-6;
+  lasttime = newtime -.001;
+
+#elif defined(HAVE_SDL)
+  newtime = SDL_GetTicks() * 1.e-3;
+  lasttime = newtime -.001;
+
+#else
+# error "We have no way to determine the time on this system."
+#endif
+  elapsedtime = .001;
+}
+
+double GetElapsedTime() {
+  return elapsedtime;
+}
+
+void UpdateTime() {
+#ifdef WIN32
+  QueryPerformanceCounter((LARGE_INTEGER*)&newtime);
+  elapsedtime = ((double)(newtime-ttime))/freq;
+  ttime = newtime;
+  if( freq==0)
+	  dblnewtime = 0.;
+  else
+	  dblnewtime = ((double)newtime)/((double)freq);
+  static double ftime = firsttime = dblnewtime;
+#elif defined(HAVE_GETTIMEOFDAY)
+  struct timeval tv;
+  (void) gettimeofday(&tv, NULL);
+
+  lasttime = newtime;
+  newtime = (double)tv.tv_sec + (double)tv.tv_usec * 1.e-6;
+  elapsedtime = newtime-lasttime;
+  static double ftime = firsttime = newtime;
+#elif defined(HAVE_SDL)
+  lasttime = newtime;
+  newtime = SDL_GetTicks() * 1.e-3;
+  elapsedtime = newtime-lasttime;
+  static double ftime = firsttime = newtime;
+#else
+# error "We have no way to determine the time on this system."
+#endif
+
+  elapsedtime *=timecompression;
+}
---
+++ prog/apps/benchmark/vanilla.cpp
@@ -0,0 +1,188 @@
+#include "brook.hpp"
+#include "lin_time.h"
+static void floatMath (float a, float b, float4 c,
+                    float d[10][10], float &e) {
+   float g=a+b;
+   float h=b*b;
+   float i = g*h+a*b;
+   float j = a>.5?g*i/(h>.001?h:1.0)+h:g*i*h-h;
+   float k = g*i*h+h;
+   float l = g+h*g+k*i;
+   float m = g+j*k-i*k/(h>.001?h:1.0);
+   float o = k;
+   o+=5;
+   g+=9;
+   h*=24;
+   i*=1;
+   o+=0+0+0+0+0+0+0+0+0+0+0+0+0+0+0+0+0;
+   o+=21049;
+   o+=h*i+g+i-m*(l-j)/(k>.001?k:1.0);
+   e= g+h*i/(k>.001?k:1.0)+m*l*1*1*1*1*1*1*1*1*1*1*1;
+}
+static void math(float a, float b, float4 c,
+                    float d[10][10], float &e) {
+   float4 tmp={a+b,a*b,(a+b)*(b*a),a*c.x};
+   float4 g;
+   float f;
+
+   g=tmp;
+   tmp.x*=c.x+tmp.w;
+   tmp.y*=c.y+tmp.w;
+   tmp.z*=c.z+tmp.w;
+   tmp.w*=c.w+tmp.w;
+   f=tmp.x+tmp.y+tmp.z+tmp.w;
+   tmp.x=f;tmp.y=f;tmp.z=f;tmp.w=f;
+   e=f+(tmp.x+g.x)+(tmp.y+g.y)+(tmp.z+g.z);
+}
+
+static void gather (float a, float b, float4 c,
+                    float d[10][10], float &e) {
+  e = a+b+d[(int)c.y][(int)c.x]+d[(int)c.z][(int)c.y]+d[(int)c.w][(int)c.z]
+     +d[(int)c.z][(int)c.x]+d[(int)c.w][(int)c.x]+d[(int)c.w][(int)c.y]
+     +d[(int)c.w][(int)c.z];
+}
+
+static void  FloatMath (const __BrtFloat1  &a,
+                         const __BrtFloat1  &b,
+                         const __BrtFloat4  &c,
+                         const __BrtArray<__BrtFloat1  , 2  , false> &d,
+                         __BrtFloat1  &e){
+  __BrtFloat1  g = a + b;
+  __BrtFloat1  h = b * b;
+  __BrtFloat1  i = g * h + a * b;
+  __BrtFloat1  j = (a > __BrtFloat1(0.5)).questioncolon(g * i / ((h > __BrtFloat1(0.001)).questioncolon(h,__BrtFloat1(1))) + h,g * i * h - h);
+  __BrtFloat1  k = g * i * h + h;
+  __BrtFloat1  l = g + h * g + k * i;
+  __BrtFloat1  m = g + j * k - i * k / ((h > __BrtFloat1(0.001)).questioncolon(h,__BrtFloat1(1)));
+  __BrtFloat1  o = k;
+
+  o += __BrtInt1(5);
+  g += __BrtInt1(9);
+  h *= __BrtInt1(24);
+  i *= __BrtInt1(1);
+  o += __BrtInt1(0) + __BrtInt1(0) + __BrtInt1(0) + __BrtInt1(0) + __BrtInt1(0) + __BrtInt1(0) + __BrtInt1(0) + __BrtInt1(0) + __BrtInt1(0) + __BrtInt1(0) + __BrtInt1(0) + __BrtInt1(0) + __BrtInt1(0) + __BrtInt1(0) + __BrtInt1(0) + __BrtInt1(0) + __BrtInt1(0);
+  o += __BrtInt1(21049);
+  o += h * i + g + i - m * (l - j) / ((k > __BrtFloat1(0.001)).questioncolon(k,__BrtFloat1(1)));
+  e = g + h * i / ((k > __BrtFloat1(0.001)).questioncolon(k,__BrtFloat1(1))) + m * l * __BrtInt1(1) * __BrtInt1(1) * __BrtInt1(1) * __BrtInt1(1) * __BrtInt1(1) * __BrtInt1(1) * __BrtInt1(1) * __BrtInt1(1) * __BrtInt1(1) * __BrtInt1(1) * __BrtInt1(1);
+}
+
+static void  Math(const __BrtFloat1  &a,
+                   const __BrtFloat1  &b,
+                   const __BrtFloat4  &c,
+                   const __BrtArray<__BrtFloat1  , 2  , false> &d,
+                   __BrtFloat1  &e){
+   __BrtFloat4  tmp;
+  __BrtFloat4  g;
+  __BrtFloat1  f;
+
+  tmp = (a + b , a * b , (a + b) * (b * a) , a * c.swizzle1(maskX));
+  g = tmp;
+  tmp *= c + tmp;
+  tmp = f.mask4(tmp.swizzle1(maskX) + tmp.swizzle1(maskY) + tmp.swizzle1(maskZ) + tmp.swizzle1(maskW),maskX,maskX,maskX,maskX);
+  e = f + (tmp + g).swizzle1(maskX) + (tmp + g).swizzle1(maskY) + (tmp + g).swizzle1(maskZ);
+}
+ 
+
+void  Gather (const __BrtFloat1  &a,
+               const __BrtFloat1  &b,
+               const __BrtFloat4  &c,
+               const __BrtArray<__BrtFloat1  , 2  , false> &d,
+               __BrtFloat1  &e){
+  e = a + b + d[c.swizzle2(maskX, maskY)].gather() + d[c.swizzle2(maskY, maskZ)].gather() + d[c.swizzle2(maskZ, maskW)].gather() + d[c.swizzle2(maskX, maskZ)].gather() + d[c.swizzle2(maskX, maskW)].gather() + d[c.swizzle2(maskY, maskW)].gather() + d[c.swizzle2(maskZ, maskW)].gather();
+}
+
+void cputest(float *a, float *b, float4 c, float * d, float * e, unsigned int size) {
+   unsigned int total=size*size;
+   float * aa=a;
+   float* bb = b; 
+   float (*dd)[10] = (float (*)[10])d;
+   float * ee=e;
+   unsigned int i;
+   UpdateTime();
+   for (i=0;i<total;++i) {
+      gather (*aa++,*bb++,c,dd,*ee++);
+   }
+   UpdateTime();
+   fprintf (stderr,"Native Gather took %lf seconds\n",GetElapsedTime());
+   aa=a;bb = b;ee=e;
+   UpdateTime();
+   for (i=0;i<total;++i) {
+      math (*aa++,*bb++,c,dd,*ee++);
+   }
+   UpdateTime();
+   fprintf (stderr,"Native Math took %lf seconds\n",GetElapsedTime());
+
+   aa=a;bb = b;ee=e;
+   UpdateTime();
+   for (i=0;i<total;++i) {
+      floatMath (*aa++,*bb++,c,dd,*ee++);
+   }
+   UpdateTime();
+   fprintf (stderr,"Native Float Math took %lf seconds\n",GetElapsedTime());
+
+
+
+
+   {
+      __BrtFloat1 * aaa=(__BrtFloat1*)a;
+      __BrtFloat1* bbb = (__BrtFloat1*)b; 
+      __BrtFloat1* d_data =(__BrtFloat1*)d;
+      unsigned int extents[2]={size,size};
+      __BrtArray<__BrtFloat1,2,false> dd (d_data,&extents[0]);
+      __BrtFloat1* eee=(__BrtFloat1*)e;
+      __BrtFloat4* cc = (__BrtFloat4*)&c;
+      __BrtFloat1* aa=aaa;__BrtFloat1 *bb=bbb; __BrtFloat1 *ee=eee;
+   UpdateTime();
+   for (i=0;i<total;++i) {
+      Gather (*aa,*bb,*cc,dd,*ee);
+   }
+   UpdateTime();
+   fprintf (stderr,"Same Memory BRT Gather took %lf seconds\n",GetElapsedTime());
+   aa=aaa;bb = bbb;ee=eee;
+   UpdateTime();
+   for (i=0;i<total;++i) {
+      Math (*aa,*bb,*cc,dd,*ee);
+   }
+   UpdateTime();
+   fprintf (stderr,"Same Memory BRT Math took %lf seconds\n",GetElapsedTime());
+
+   aa=aaa;bb = bbb;ee=eee;
+   UpdateTime();
+   for (i=0;i<total;++i) {
+      FloatMath (*aa,*bb,*cc,dd,*ee);
+   }
+   UpdateTime();
+   fprintf (stderr,"Same Memory Float Math took %lf seconds\n",GetElapsedTime());
+
+   }
+
+
+   UpdateTime();
+   for (i=0;i<total;++i) {
+      gather (*aa,*bb,c,dd,*ee);
+   }
+   UpdateTime();
+   fprintf (stderr,"Same Memory Native Gather took %lf seconds\n",GetElapsedTime());
+   aa=a;bb = b;ee=e;
+   UpdateTime();
+   for (i=0;i<total;++i) {
+      math (*aa,*bb,c,dd,*ee);
+   }
+   UpdateTime();
+   fprintf (stderr,"Same Memory Native Math took %lf seconds\n",GetElapsedTime());
+
+   aa=a;bb = b;ee=e;
+   UpdateTime();
+   for (i=0;i<total;++i) {
+      floatMath (*aa,*bb,c,dd,*ee);
+   }
+   UpdateTime();
+   fprintf (stderr,"Same Memory Native Float Math took %lf seconds\n",GetElapsedTime());
+
+}
+
+
+
+
+
+
