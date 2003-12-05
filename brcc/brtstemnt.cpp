2114cc8b900fd4de2a3fd3d4a3245101b7defefb
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2003-12-05 20:58:38 +0000

    ok gcc is behind the times for once

--- brcc/brtstemnt.cpp
+++ brcc/brtstemnt.cpp
@@ -91,7 +91,9 @@ BRTKernelDef::print(std::ostream& out, int) const
    printStub(out);
 #undef PRINT_IF_NEEDED
    if (decl->isReduce()) {
-      BRTScatterDef(*BRTCPUReduceCode(*this).fDef).print(out,0);
+     BRTCPUReduceCode crc(*this);
+      BRTScatterDef sd(*crc.fDef);
+      sd.print(out,0);
    }
 }
 
