d17aa25413a3ec3b8b04a8ce0ffc3867e3b41918
Timothy J. Foley <timfoleysama@users.sourceforge.net>
2004-01-14 22:51:15 +0000

    Did some timing runs of the new mout app.

--- prog/apps/particle_cloth/main.cpp
+++ prog/apps/particle_cloth/main.cpp
@@ -8,12 +8,14 @@
 
 void cpuIterate( int, int, int& );
 void gpuIterate( int, int, int* );
+void moutIterate( int, int, int* );
 
 int main( int argc, char** argv )
 {
   SetupMillisTimer();
 
   int time;
+  bool mout = false;
 
   if( argc < 4 )
   {
@@ -28,22 +30,56 @@ int main( int argc, char** argv )
   {
     cpuIterate( size, iterations, time );
   }
+  else if( strcmp( argv[1], "mout" ) == 0 )
+  {
+    moutIterate( size, iterations, &time );
+	mout = true;
+  }
   else
   {
     gpuIterate( size, iterations, &time );
   }
 
+  double totalOps;
 
-  double n = (double)size;
-  double stepOps = ((n*n - 2)*21+1);
-  double windOps = (4*(n-1)*(n-1)*23);
-  double springOps = (2*n*(n-1) + 2*(n-1)*(n-1) + 2*n*(n-2))*34;
-  double totalOps = ( stepOps + windOps + springOps ) * iterations;
+  double onewayBandwidth = 0;
+  double twowayBandwidth = 0;
+
+  if( mout )
+  {
+	  double n = (double)size;
+
+	  // 26 ops per spring
+	  double springOps = 2*n*(n-1)*26;
+	  // 23 ops per step
+	  double stepOps = n*n*23;
+	  // 9 misc ops
+	  double miscOps = n*n*9;
+	  totalOps = (springOps + stepOps + miscOps) * iterations;
+
+	  onewayBandwidth = 6*4*4*( n * n * iterations ) / (double)time;
+	  twowayBandwidth = 2*5*4*4*( n * n * iterations ) / (double)time;
+  }
+  else
+  {
+	double n = (double)size;
+	double stepOps = ((n*n - 2)*21+1);
+	double windOps = (4*(n-1)*(n-1)*23);
+	double springOps = (2*n*(n-1) + 2*(n-1)*(n-1) + 2*n*(n-2))*34;
+	totalOps = ( stepOps + windOps + springOps ) * iterations;
+  }
   double megaflops = totalOps / (double)time;
 
   std::cout << iterations << "\t";
   std::cout << time << "\t";
+  if( mout )
+  {
+	  std::cout << "1way: " << onewayBandwidth << "\t";
+	  std::cout << "2way: " << twowayBandwidth << "\t";
+  }
   std::cout << megaflops << std::endl;
+
+
 
   CleanupMillisTimer();
   return 0;
