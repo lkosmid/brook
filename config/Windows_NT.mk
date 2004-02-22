5b2b5c6e2e0c948f87d8642779ba3cf94d72f3a8
Ian Buck <ianbuck@users.sourceforge.net>
2004-02-22 22:00:29 +0000

    Fixed slash direction for DX libraries

--- config/Windows_NT.mk
+++ config/Windows_NT.mk
@@ -31,7 +31,7 @@ C_STATIC_FLAG    :=
 C_OUTPUT_FLAG    := /Fo
 C_COMPILE_FLAG   := /c
 
-LDFLAGS           += /nologo /map /fixed:no /INCREMENTAL:NO /libpath:/DX90SDK/LIB
+LDFLAGS           += /nologo /map /fixed:no /INCREMENTAL:NO /libpath:\\DX90SDK\\LIB
 LD_LIBDIR_FLAG    := /libpath:
 LD_SHARED_FLAG    := /DLL
 LD_OUTPUT_FLAG    := /out:
