1c4c4f4878ea12f24b6e9484520fff92eed9e02d
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2004-02-23 04:41:38 +0000

    made it work in gcc295

--- runtime/nv30gl/glfunc.cpp
+++ runtime/nv30gl/glfunc.cpp
@@ -7,12 +7,13 @@ using namespace brook;
 RUNTIME_BONUS_GL_FNS
 #undef XXX
 #endif
-
-void brook::initglfunc(void) {
+namespace brook {
+void initglfunc(void) {
 #ifdef WIN32
 #define   XXX(type, fn) fn = (type) wglGetProcAddress(#fn); assert(fn);
 RUNTIME_BONUS_GL_FNS
 #undef XXX
 #endif
+}
 }
 
