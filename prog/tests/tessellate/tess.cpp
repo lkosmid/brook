a7f349eb02a8e63197815258b79912a08f4c4c12
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2004-02-26 22:26:53 +0000

    added cheating facilities so we cna see what happens if we had this graphics hardware

--- prog/tests/tessellate/tess.cpp
+++ prog/tests/tessellate/tess.cpp
@@ -24,9 +24,16 @@ float curve (float x, float y) {
 float evaluateError(float input, float actual) {
   return fabs((input-actual)/(actual+1));
 }
+static int cheatdenom=0;
+static int cheatnumerator=0;
+bool  shouldsplitcheat (int iter) {
+  static int i=0;
+  return ((iter%2048)%cheatdenom<cheatnumerator);
+}
 void Tessellate (const vector<float4> &inputv, float toler, vector<float4> &outputv) {
   for (vector<float4>::const_iterator input=inputv.begin();input!=inputv.end();++input) {
     float heightx, heighty,centerx,centery,centerz;
+    int iter=0;
     heightx = curve(input->x,input->y);
     heighty = curve(input->z,input->w);
     centerx = .5f*input->x+.5f*input->z;
@@ -34,7 +41,7 @@ void Tessellate (const vector<float4> &inputv, float toler, vector<float4> &outp
     centerz = .5f*heightx+.5f*heighty;
     float fheight=curve(centerx,centery);
     float error=evaluateError(centerz,fheight);
-    if (error>=toler) {
+    if ((!cheatdenom)?error>=toler:shouldsplitcheat(iter++)) {
       float4 myoutput(input->x,input->y,centerx,centery);
       outputv.push_back(myoutput);
       myoutput.x=input->z;myoutput.y=input->w;      
@@ -75,9 +82,13 @@ stop=GetTimeMillis();
 return 0;
 */
 {
+  char * data=(char*)malloc((SIZE+1)*(SIZE+1)*sizeof(char));
   float toler = argc>1?tofd(atof(argv[1])):tofd(.1);
   int limit = argc>2?atoi(argv[2]):32;
+  cheatdenom=argc>3?atoi(argv[3]):0;
+  cheatnumerator=argc>4?atoi(argv[4]):1;
   float lastsize;
+  float sizes[4096]={0};
   vector <float4> polys;
   polys.push_back(float4(0,0,SIZE/2,SIZE/2));
   polys.push_back(float4(0,SIZE/2,SIZE/2,SIZE));
@@ -85,6 +96,7 @@ return 0;
   polys.push_back(float4(SIZE/2,SIZE/2,SIZE,SIZE));
   int ite;
   SetupMillisTimer();
+  if (cheatdenom) printf ("Warning: Cheating to get lower even results\n");
   start = GetTimeMillis();
   ite=0;
   do {
@@ -96,11 +108,16 @@ return 0;
       Tessellate(polys,toler,nextPolys);
       polys.swap(nextPolys);      
     }
+    if (ite<4096) sizes[ite-1]=ceil(polys.size()/2048.0f);
   }while (polys.size()/STRIDE<(unsigned int)limit
           &&polys.size()!=(unsigned int)lastsize);
   {
     float4 * finaldata=&polys[0];
-    char * data=(char*)malloc((SIZE+1)*(SIZE+1)*sizeof(char));
+    stop = GetTimeMillis();
+    for (ite=0;ite<4096&&sizes[ite];++ite) {
+      printf ("Size %f\n",sizes[ite]);
+    }
+    printf ("time %f\n",(float)(stop-start));
     memset(data,0,sizeof(char)*(1+SIZE)*(1+SIZE));
     stop = GetTimeMillis();
 
@@ -108,7 +125,6 @@ return 0;
     writePng("polys.ppm",data,SIZE+1,SIZE+1);
     free (data);   
   }
-  printf ("%f ",(float)(stop-start));
   return 0;
 }
 }
