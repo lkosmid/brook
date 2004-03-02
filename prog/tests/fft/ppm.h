86b4e1e2b164fd32dd0a90dc8420fb9dd5603e05
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2004-03-02 05:57:10 +0000

    w00t! made the ppm format more robust

--- prog/tests/fft/ppm.h
+++ prog/tests/fft/ppm.h
@@ -2,15 +2,20 @@ int readPPM(const char *filename,
 	    unsigned char **data, 
 	    unsigned int *width, 
 	    unsigned int *height) {
+  *data =0;
   char header[256];
   int nbyte;
 
   FILE *fp = fopen(filename, "rb");
-  assert (fp);
-
-  fscanf (fp, "%s", header);
+  if (!fp)return 0;
+  header[2]=0;
+  fscanf(fp,"%c%c%c",&header[0],&header[1],&header[3]);
+  
   assert (!strncmp(header, "P6", strlen("P6")));
-
+  int err;
+  do {
+     err=fscanf(fp,"#%*[^#\n]\n");
+  }while (err!=0&&err!=EOF);
   fscanf (fp, "%d %d %d\n", width, height, &nbyte);
   assert (nbyte == 255);
   assert (*width > 0 && *width <= 2048);
@@ -21,7 +26,7 @@ int readPPM(const char *filename,
   fread(*data, *width * *height, 3, fp);
   fclose(fp);
 
-  return 0;
+  return 1;
 }
 
 
