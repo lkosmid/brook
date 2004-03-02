c53f2962de941289ca160054ecf1356138f470aa
Jeremy Sugerman <jeremy_sugerman@users.sourceforge.net>
2004-03-02 00:53:36 +0000

    Rebind the program every time we define a constant register.  With this change, we can actually pass the 'constant' and 'scan' regression tests.

--- runtime/nv30gl/arbkernel.cpp
+++ runtime/nv30gl/arbkernel.cpp
@@ -8,10 +8,16 @@ void
 ARBKernel::BindParameter(const float x, const float y,
                          const float z, const float w)
 {
+#if 0
    std::cerr << "ARB: Binding (" << x << ", " << y << ", " << z << ", " << w
               << ") to parameter #" << creg << "\n";
+#endif
    for (unsigned int i=0; i < npasses; i++) {
-      //glBindProgramARB(GL_FRAGMENT_PROGRAM_ARB, pass_id[i]);
+      /*
+       * This rebinding appears to be mandatory.  At least, without it, we
+       * fail the 'constant' regression test. --Jeremy.
+       */
+      glBindProgramARB(GL_FRAGMENT_PROGRAM_ARB, pass_id[i]);
       glProgramLocalParameter4fARB(GL_FRAGMENT_PROGRAM_ARB, creg, x, y, z, w);
    }
    CHECK_GL();
