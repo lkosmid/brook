4565a6f187ab85b67b3cc62dedcd36ab89be63e3
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2004-03-29 03:40:56 +0000

    fixed a typo in the CS lib

--- prog/apps/rapid_collide/rapid_test.cpp
+++ prog/apps/rapid_collide/rapid_test.cpp
@@ -1,5 +1,7 @@
 #include <brook.hpp>
 #include <stdio.h>
+#include "csgeom/vector3.h"
+#include "csgeom/matrix3.h"
 typedef struct Tri_t {
   float3 A;
   float3 B;
@@ -30,196 +32,6 @@ typedef struct Tri_t {
 #define USE_EPSILON_TEST	1
 
 const   float EPSILON=.000001f;
-float ABS (float x) {
-  return x>=0?x:-x;
-}
-
-class csVector3
-{
-public:
-  /// The X component of the vector
-  float x;
-  /// The Y component of the vector
-  float y;
-  /// The Z component of the vector
-  float z;
-
-  /**
-   * Make a new vector. The vector is not
-   * initialized. This makes the code slightly faster as
-   * csVector3 objects are used a lot.
-   */
-  csVector3 () {}
-
-  /**
-   * Make a new initialized vector.
-   * Creates a new vector and initializes it to m*<1,1,1>.  To create
-   * a vector initialized to the zero vector, use csVector3(0)
-   */
-  csVector3 (float3 n): x(n.x), y(n.y), z(n.z){}
-  csVector3 (float m) : x(m), y(m), z(m) {}
-
-  /// Make a new vector and initialize with the given values.
-  csVector3 (float ix, float iy, float iz = 0) : x(ix), y(iy), z(iz) {}
-
-  /// Copy Constructor.
-  csVector3 (const csVector3& v) : x(v.x), y(v.y), z(v.z) {}
-  /// Conversion from double precision vector to single.
-  /// Add two vectors.
-  inline csVector3 operator+ (const csVector3& v2) const
-  { return csVector3(x+v2.x, y+v2.y, z+v2.z); }
-
-  /// Subtract two vectors.
-  inline csVector3 operator- (const csVector3& v2) const
-  { return csVector3(x-v2.x, y-v2.y, z-v2.z); }
-
-  /// Subtract two vectors of differing type, cast to double.
-
-  /// Subtract two vectors of differing type, cast to double.
-
-  /// Take the dot product of two vectors.
-  inline friend float operator* (const csVector3& v1, const csVector3& v2)
-  { return v1.x*v2.x + v1.y*v2.y + v1.z*v2.z; }
-
-  /// Take the cross product of two vectors.
-  inline friend csVector3 operator% (const csVector3& v1, const csVector3& v2)
-  {
-    return csVector3 (v1.y*v2.z-v1.z*v2.y,
-                      v1.z*v2.x-v1.x*v2.z,
-                      v1.x*v2.y-v1.y*v2.x);
-  }
-
-  /// Take cross product of two vectors and put result in this vector.
-  void Cross (const csVector3 & px, const csVector3 & py)
-  {
-    x = px.y*py.z - px.z*py.y;
-    y = px.z*py.x - px.x*py.z;
-    z = px.x*py.y - px.y*py.x;
-  }
-
-  /// Multiply a vector and a scalar.
-  inline friend csVector3 operator* (const csVector3& v, float f)
-  { return csVector3(v.x*f, v.y*f, v.z*f); }
-
-  /// Multiply a vector and a scalar.
-  inline friend csVector3 operator* (float f, const csVector3& v)
-  { return csVector3(v.x*f, v.y*f, v.z*f); }
-
-
-  /// Multiply a vector and a scalar int.
-  inline friend csVector3 operator* (const csVector3& v, int f)
-  { return v * (float)f; }
-
-  /// Multiply a vector and a scalar int.
-  inline friend csVector3 operator* (int f, const csVector3& v)
-  { return v * (float)f; }
-
-  /// Divide a vector by a scalar.
-  inline friend csVector3 operator/ (const csVector3& v, float f)
-  { f = 1.0f/f; return csVector3(v.x*f, v.y*f, v.z*f); }
-
-
-  /// Divide a vector by a scalar int.
-  inline friend csVector3 operator/ (const csVector3& v, int f)
-  { return v / (float)f; }
-
-  /// Check if two vectors are equal.
-  inline friend bool operator== (const csVector3& v1, const csVector3& v2)
-  { return v1.x==v2.x && v1.y==v2.y && v1.z==v2.z; }
-
-  /// Check if two vectors are not equal.
-  inline friend bool operator!= (const csVector3& v1, const csVector3& v2)
-  { return v1.x!=v2.x || v1.y!=v2.y || v1.z!=v2.z; }
-
-  /// Project one vector onto another.
-  inline friend csVector3 operator>> (const csVector3& v1, const csVector3& v2)
-  { return v2*(v1*v2)/(v2*v2); }
-
-  /// Project one vector onto another.
-  inline friend csVector3 operator<< (const csVector3& v1, const csVector3& v2)
-  { return v1*(v1*v2)/(v1*v1); }
-
-  /// Test if each component of a vector is less than a small epsilon value.
-  inline friend bool operator< (const csVector3& v, float f)
-  { return ABS(v.x)<f && ABS(v.y)<f && ABS(v.z)<f; }
-
-  /// Test if each component of a vector is less than a small epsilon value.
-  inline friend bool operator> (float f, const csVector3& v)
-  { return ABS(v.x)<f && ABS(v.y)<f && ABS(v.z)<f; }
-
-  /// Returns n-th component of the vector.
-  inline float operator[] (int n) const { return !n?x:n&1?y:z; }
-
-  /// Returns n-th component of the vector.
-  inline float & operator[] (int n) { return !n?x:n&1?y:z; }
-
-  /// Add another vector to this vector.
-  inline csVector3& operator+= (const csVector3& v)
-  {
-    x += v.x;
-    y += v.y;
-    z += v.z;
-
-    return *this;
-  }
-
-  /// Subtract another vector from this vector.
-  inline csVector3& operator-= (const csVector3& v)
-  {
-    x -= v.x;
-    y -= v.y;
-    z -= v.z;
-
-    return *this;
-  }
-
-  /// Multiply this vector by a scalar.
-  inline csVector3& operator*= (float f)
-  { x *= f; y *= f; z *= f; return *this; }
-
-  /// Divide this vector by a scalar.
-  inline csVector3& operator/= (float f)
-  { f = 1.0f / f; x *= f; y *= f; z *= f; return *this; }
-
-  /// Unary + operator.
-  inline csVector3 operator+ () const { return *this; }
-
-  /// Unary - operator.
-  inline csVector3 operator- () const { return csVector3(-x,-y,-z); }
-
-  /// Set the value of this vector.
-  inline void Set (float sx, float sy, float sz) { x = sx; y = sy; z = sz; }
-
-  /// Set the value of this vector.
-  inline void Set (const csVector3& v) { x = v.x; y = v.y; z = v.z; }
-
-  /// Returns the norm of this vector.
-  float Norm () const;
-
-  /// Return the squared norm (magnitude) of this vector.
-  float SquaredNorm () const
-  { return x * x + y * y + z * z; }
-
-  /**
-   * Returns the unit vector in the direction of this vector.
-   * Attempting to normalize a zero-vector will result in a divide by
-   * zero error.  This is as it should be... fix the calling code.
-   */
-  csVector3 Unit () const { return (*this)/(this->Norm()); }
-
-  /// Returns the norm (magnitude) of a vector.
-  inline static float Norm (const csVector3& v) { return v.Norm(); }
-
-  /// Normalizes a vector to a unit vector.
-  inline static csVector3 Unit (const csVector3& v) { return v.Unit(); }
-
-  /// Scale this vector to length = 1.0;
-  void Normalize ();
-
-  /// Query if the vector is zero
-  inline bool IsZero () const
-  { return (x == 0) && (y == 0) && (z == 0); }
-};
 
 /* some macros */
 #define CROSS(dest, v1, v2)			\
@@ -603,6 +415,135 @@ extern void doTestNoDiv(float * matrix,
                         float3 * hitsa,
                         float3* hitsb,
                         float4 * ohits);
+int obb_disjoint (const csMatrix3& B, const csVector3& T,
+	const csVector3& a, const csVector3& b)
+{
+  register float t, s;
+  register int r;
+  csMatrix3 Bf;
+  const float reps = 1e-6;
+
+  // Bf = ABS (B)
+  Bf.m11 = ABS (B.m11);  Bf.m11 += reps;
+  Bf.m12 = ABS (B.m12);  Bf.m12 += reps;
+  Bf.m13 = ABS (B.m13);  Bf.m13 += reps;
+  Bf.m21 = ABS (B.m21);  Bf.m21 += reps;
+  Bf.m22 = ABS (B.m22);  Bf.m22 += reps;
+  Bf.m23 = ABS (B.m23);  Bf.m23 += reps;
+  Bf.m31 = ABS (B.m31);  Bf.m31 += reps;
+  Bf.m32 = ABS (B.m32);  Bf.m32 += reps;
+  Bf.m33 = ABS (B.m33);  Bf.m33 += reps;
+
+  // if any of these tests are one-sided, then the polyhedra are disjoint
+  r = 1;
+
+  // A1 x A2 = A0
+  t = ABS (T.x);
+
+  r &= (t <= (a.x + b.x * Bf.m11 + b.y * Bf.m12 + b.z * Bf.m13));
+  if (!r) return 1;
+
+  // B1 x B2 = B0
+  s = T.x * B.m11 + T.y * B.m21 + T.z * B.m31;
+  t = ABS (s);
+
+  r &= (t <= (b.x + a.x * Bf.m11 + a.y * Bf.m21 + a.z * Bf.m31));
+  if (!r) return 2;
+
+  // A2 x A0 = A1
+  t = ABS (T.y);
+
+  r &= (t <= (a.y + b.x * Bf.m21 + b.y * Bf.m22 + b.z * Bf.m23));
+  if (!r) return 3;
+
+  // A0 x A1 = A2
+  t = ABS (T.z);
+
+  r &= (t <= (a.z + b.x * Bf.m31 + b.y * Bf.m32 + b.z * Bf.m33));
+  if (!r) return 4;
+
+  // B2 x B0 = B1
+  s = T.x * B.m12 + T.y * B.m22 + T.z * B.m32;
+  t = ABS (s);
+
+  r &= (t <= (b.y + a.x * Bf.m12 + a.y * Bf.m22 + a.z * Bf.m32));
+  if (!r) return 5;
+
+  // B0 x B1 = B2
+  s = T.x * B.m13 + T.y * B.m23 + T.z * B.m33;
+  t = ABS (s);
+
+  r &= (t <= (b.z + a.x * Bf.m13 + a.y * Bf.m23 + a.z * Bf.m33));
+  if (!r) return 6;
+
+  // A0 x B0
+  s = T.z * B.m21 - T.y * B.m31;
+  t = ABS (s);
+
+  r &= (t <= (a.y * Bf.m31 + a.z * Bf.m21 + b.y * Bf.m13 + b.z * Bf.m12));
+  if (!r) return 7;
+
+  // A0 x B1
+  s = T.z * B.m22 - T.y * B.m32;
+  t = ABS (s);
+
+  r &= (t <= (a.y * Bf.m32 + a.z * Bf.m22 + b.x * Bf.m13 + b.z * Bf.m11));
+  if (!r) return 8;
+
+  // A0 x B2
+  s = T.z * B.m23 - T.y * B.m33;
+  t = ABS (s);
+
+  r &= (t <= (a.y * Bf.m33 + a.z * Bf.m23 + b.x * Bf.m12 + b.y * Bf.m11));
+  if (!r) return 9;
+
+  // A1 x B0
+  s = T.x * B.m31 - T.z * B.m11;
+  t = ABS (s);
+
+  r &= (t <= (a.x * Bf.m31 + a.z * Bf.m11 + b.y * Bf.m23 + b.z * Bf.m22));
+  if (!r) return 10;
+
+  // A1 x B1
+  s = T.x * B.m32 - T.z * B.m12;
+  t = ABS (s);
+
+  r &= (t <= (a.x * Bf.m32 + a.z * Bf.m12 + b.x * Bf.m23 + b.z * Bf.m21));
+  if (!r) return 11;
+
+  // A1 x B2
+  s = T.x * B.m33 - T.z * B.m13;
+  t = ABS (s);
+
+  r &= (t <= (a.x * Bf.m33 + a.z * Bf.m13 + b.x * Bf.m22 + b.y * Bf.m21));
+  if (!r) return 12;
+
+  // A2 x B0
+  s = T.y * B.m11 - T.x * B.m21;
+  t = ABS (s);
+
+  r &= (t <= (a.x * Bf.m21 + a.y * Bf.m11 + b.y * Bf.m33 + b.z * Bf.m32));
+  if (!r) return 13;
+
+  // A2 x B1
+  s = T.y * B.m12 - T.x * B.m22;
+  t = ABS (s);
+
+  r &= (t <= (a.x * Bf.m22 + a.y * Bf.m12 + b.x * Bf.m33 + b.z * Bf.m31));
+  if (!r) return 14;
+
+  // A2 x B2
+  s = T.y * B.m13 - T.x * B.m23;
+  t = ABS (s);
+
+  r &= (t <= (a.x * Bf.m23 + a.y * Bf.m13 + b.x * Bf.m32 + b.y * Bf.m31));
+  if (!r) return 15;
+
+  return 0;  // should equal 0
+}
+
+extern float TestObbDisjoint(float3 X, float3 Y, float3 Z, float3 T,
+                             float3 a, float3 b);
 #define SIZEX 1
 #define SIZEY 1
 void SetTriangles(int size, float3* a, float3 *b) {
@@ -616,6 +557,31 @@ void SetTriangles(int size, float3* a, float3 *b) {
   b[i*3+2]=float3(-1-rand()/rm,-1-rand()/rm,-1-rand()/rm);
   }
 }
+float3 convertVec(csVector3 v){
+  return float3 (v.x,v.y,v.z);
+}
+
+extern float TestObbDisjoint (float3 X, float3 Y, float3 Z, float3 T,
+                              float3  a, float3 b);
+void testOBB() {
+  for (unsigned int i=0;i<1024*32;++i) {
+
+  float rm = RAND_MAX;
+  csMatrix3 mat (rand()/rm,rand()/rm,rand()/rm,
+                 rand()/rm,rand()/rm,rand()/rm,
+                 rand()/rm,rand()/rm,rand()/rm);
+  float3 T (rand()/rm*3-1.5,rand()/rm*3-1.5,rand()/rm*3-1.5);
+  
+  float3 bX = convertVec(mat.Row1());
+  float3 bY = convertVec(mat.Row2());
+  float3 bZ = convertVec(mat.Row3());
+  float3 r1 (rand()/rm,rand()/rm,rand()/rm);
+  float3 r2 (rand()/rm,rand()/rm,rand()/rm);
+  bool cputest = obb_disjoint(mat,T,r1,r2)?1:0;
+  bool brooktest = TestObbDisjoint(bX,bY,bZ,T,r1,r2)?1:0;
+  assert(cputest==brooktest);
+  } 
+}
 int main (int argc, char ** argv) {
   float3 * a= (float3*)malloc(sizeof(float3)*3*SIZEX*SIZEY);
   float3 * b= (float3*)malloc(sizeof(float3)*3*SIZEX*SIZEY);
@@ -626,12 +592,13 @@ int main (int argc, char ** argv) {
                     0,0,1,0,
                     0,0,0,1};
   srand(1);
+  testOBB();
   for (unsigned int k=0;k<12;++k) {
   SetTriangles(SIZEX*SIZEY,a,b);
   }
   int counter=0;
   int counter2=0;
-  for (unsigned int j=0;j<1024*64;++j) {
+  for (unsigned int j=0;j<1024*16;++j) {
   SetTriangles(SIZEX*SIZEY,a,b);
   doTest(matrix,SIZEX,SIZEY,a,b,rez);
   doTestNoDiv(matrix,SIZEX,SIZEY,a,b,rez_nodiv);
