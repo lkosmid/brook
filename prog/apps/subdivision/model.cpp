c6fa9c11a2e95abdee3dd915681727d9b3bd2e22
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2004-04-02 08:41:08 +0000

    fixed subdivision a lot..prolly still wont work

--- prog/apps/subdivision/model.cpp
+++ prog/apps/subdivision/model.cpp
@@ -19,14 +19,17 @@ unsigned int loadModelData(const char * filename,
                            Neighbor ** neigh) {
    std::vector<Tri>tList;
    LoadPly(filename,tList);
-   
+   *tri = (Tri*)malloc(sizeof(Tri)*tList.size());
+   *neigh =(Neighbor*)malloc(sizeof(Neighbor)*tList.size());
    for (unsigned int i=0;i<tList.size();++i) {
       unsigned int j;
+      float4 zero4(0,0,0,0);
       Tri t = tList[i];
       t.A.w=0;
       t.B.w=0;
       t.C.w=0;
       Neighbor n;
+      memset(&n,0,sizeof(Neighbor));
       for (j=0;j<tList.size();++j) {
          //check for AB, BC, AC
          Tri nei=tList[j];
@@ -43,8 +46,28 @@ unsigned int loadModelData(const char * filename,
          checkEdgeNeighbor(t.C,n.BC,n.BCC,nei);
          checkEdgeNeighbor(t.C,n.AC,n.ACC,nei);
       }
+      if (n.ABB==n.BBC)
+        n.BBC=zero4;
+      if (n.BCC==n.ACC)
+        n.ACC=zero4;
+      if (n.AAB==n.AAC)
+        n.AAB=zero4;
+      if (n.AB==n.ABB)
+        n.ABB=zero4;
+      if (n.AAB==n.AB)
+        n.AAB=zero4;
+      if (n.AAC==n.AC)
+        n.AAC=zero4;
+      if (n.ACC==n.AC)
+        n.ACC=zero4;
+      if (n.BBC==n.BC)
+        n.BBC=zero4;
+      if (n.BCC==n.BC)
+        n.BBC=zero4;
+      (*tri)[i]=t;
+      (*neigh)[i]=n;
    }
-   return 0;
+   return tList.size();
 }
 
 
