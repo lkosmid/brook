cc4f99014ca688d24a5dd8b0f76d3633225fe62a
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2004-04-20 18:34:23 +0000

    final cost analysis

--- scripts/costAnalysis.py
+++ scripts/costAnalysis.py
@@ -3,21 +3,91 @@ vout={"InitialScan":1,
 	  "Scan":1,
 	  "BeginGatherSearch":1,
 	  "GatherSearch":1,
-	  "RelativeGather":1}
+	  "RelativeGather":1,
+	  "InitialScan2":1,
+	  "Scan2":1,
+	  "BeginGatherSearch2":1,
+	  "GatherSearch2":1,
+	  "RelativeGather2":1,
+	  "InitialScan3":1,
+	  "Scan3":1,
+	  "BeginGatherSearch3":1,
+	  "GatherSearch3":1,
+	  "RelativeGather3":1,
+	  "InitialScan4":1,
+	  "Scan4":1,
+	  "BeginGatherSearch4":1,
+	  "GatherSearch4":1,
+	  "RelativeGather4":1}
+voutscan = {"InitialScan":1,
+			"Scan":1,
+			"InitialScan2":1,
+			"Scan2":1,
+			"InitialScan3":1,
+			"Scan3":1,
+			"InitialScan4":1,
+			"Scan4":1
+	}
 voutscatter={"InitialScan":1,
 			 "Scan":1,
-			 "RelativeGather":1}
-costs= {"InitialScan":(16,0,4,{"add":1,
+			 "RelativeScatter":1,
+			 "InitialScan2":1,
+			 "Scan2":1,
+			 "RelativeScatter2":1,
+			 "InitialScan3":1,
+			 "Scan3":1,
+			 "RelativeScatter3":1,
+			 "InitialScan4":1,
+			 "Scan4":1,
+			 "RelativeScatter4":1}
+costs= {"InitialScan4":(32,0,4,{"add":1,
+							 "cmp":3}),
+		"Scan4":(8,0,4,{"add":1,
+					 "cmp":1}),
+		"BeginGatherSearch4":(24,0,4,{"cmp":4,
+								   "bit":1,
+								   "mul":1}),
+		"GatherSearch4":(4,20,4,{"add":3,
+								"cmp":3,
+								"mul":1}),
+		"RelativeGather4":(4,16,16,{"add":1}),
+		"RelativeScatter4":(4,16,16,{"add":1}),
+		"InitialScan3":(24,0,4,{"add":1,
+							 "cmp":3}),
+		"Scan3":(8,0,4,{"add":1,
+					 "cmp":1}),
+		"BeginGatherSearch3":(20,0,4,{"cmp":4,
+								   "bit":1,
+								   "mul":1}),
+		"GatherSearch3":(4,16,4,{"add":3,
+								"cmp":3,
+								"mul":1}),
+		"RelativeGather3":(4,12,12,{"add":1}),
+		"RelativeScatter3":(4,12,12,{"add":1}),
+		"InitialScan2":(16,0,4,{"add":1,
+							 "cmp":3}),
+		"Scan2":(8,0,4,{"add":1,
+					 "cmp":1}),
+		"BeginGatherSearch2":(16,0,4,{"cmp":4,
+								   "bit":1,
+								   "mul":1}),
+		"GatherSearch2":(4,12,4,{"add":3,
+								"cmp":3,
+								"mul":1}),
+		"RelativeGather2":(4,8,8,{"add":1}),
+		"RelativeScatter2":(4,8,8,{"add":1}),
+		"InitialScan":(8,0,4,{"add":1,
 							 "cmp":3}),
 		"Scan":(8,0,4,{"add":1,
 					 "cmp":1}),
-		"BeginGatherSearch":(20,0,4,{"cmp":4,
+		"BeginGatherSearch":(8,0,4,{"cmp":4,
 								   "bit":1,
 								   "mul":1}),
-		"GatherSearch":(4,20,4,{"add":3,
+		"GatherSearch":(4,8,4,{"add":3,
 								"cmp":3,
 								"mul":1}),
-		"RelativeGather":(4,16,16,{"add":1}),
+		"RelativeGather":(4,4,4,{"add":1}),
+		"RelativeScatter":(4,4,4,{"add":1}),		
 		"processSlice":(8,0,16,{"cmp":10,
 								"add":7,
 								"mul":1}),
@@ -35,6 +105,10 @@ costs= {"InitialScan":(16,0,4,{"add":1,
 									   "add":12*6+12*3,
 									   "cmp":12*3+12,
 									   "bit":12}),
+		"smallEnough":(192,0,192,{"add":102,
+								  "mul":12,
+								  "cmp":21,
+								  "bit":9}),
 		"splitTriangles":(8,192,96,{"add":60,
 									"mul":63,
 									"cmp":15,
@@ -62,15 +136,27 @@ costs= {"InitialScan":(16,0,4,{"add":1,
 										"cmp":3,
 										"bit":1})}
 		
-invocations={}
-time = {}
 alias = {"NanToBoolRight":"InitialScan",
 		 "NanToRight":"Scan",
 		 "linearReorgSplitTriangles":"ReorgSplitTriangles",
 		 "EstablishGuess":"BeginGatherSearch",
-		 "UpdateGuess":"GatherSearch"}
+		 "UpdateGuess":"GatherSearch",
+		 "NanToBoolRight2":"InitialScan2",
+		 "NanToRight2":"Scan2",
+		 "EstablishGuess2":"BeginGatherSearch2",
+		 "UpdateGuess2":"GatherSearch2",
+		 "NanToBoolRight3":"InitialScan3",
+		 "NanToRight3":"Scan3",
+		 "EstablishGuess3":"BeginGatherSearch3",
+		 "UpdateGuess3":"GatherSearch3",
+		 "NanToBoolRight4":"InitialScan4",
+		 "NanToRight4":"Scan4",
+		 "EstablishGuess4":"BeginGatherSearch4",
+		 "UpdateGuess4":"GatherSearch4"}
 
 for arg in sys.argv[1:]:
+	invocations={}
+	time = {}
 	fp = open(arg,"r");
 	lines = fp.readlines();
 	for line in lines:
@@ -89,6 +175,10 @@ for arg in sys.argv[1:]:
 	voutwrites=0
 	voutarith=0
 	voutmem=0
+	voutscanreads=0
+	voutscanwrites=0
+	voutscanarith=0
+	voutscanmem=0
 	voutscatreads=0
 	voutscatwrites=0
 	voutscatarith=0
@@ -120,13 +210,20 @@ for arg in sys.argv[1:]:
 			voutscatwrites+=writes
 			voutscatmem+=mem
 			voutscatarith+=arith
-		if (not func in vout):
+		if (func in voutscan):
+			voutscanreads+=reads
+			voutscanwrites+=writes
+			voutscanmem+=mem
+			voutscanarith+=arith
+		if ((not func in vout) and (not func in voutscatter) and (not func in voutscan)):
 			procreads+=reads
 			procwrites+=writes
 			procmem+=mem
 			procarith+=arith
 		#print st
 	print arg+" "+str (procmem)+" "+str (voutmem)+" "+str (voutscatmem)+" "+str (procarith)+" "+str (voutarith)+" "+str (voutscatarith);
+	print "Totals-"+arg+" "+str (procmem)+" "+str (procmem+voutmem)+" "+str (procmem+voutscatmem)+" "+str (procarith)+" "+str (procarith+voutarith)+" "+str (procarith+voutscatarith);
+	print "VoutScan-"+arg+" "+str (procmem)+" "+str (procmem+voutmem)+" "+str (procmem+voutscanmem)+" "+str (procarith)+" "+str (procarith+voutarith)+" "+str (procarith+voutscanarith);
 #	print "Totals"
 #	print "Mem: "+str (procmem+voutmem)
 #	print "Ath: "+str (procarith+voutarith)
