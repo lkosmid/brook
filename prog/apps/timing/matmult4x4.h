f13d569883eccf4434d75c6f01e6b4b1b2458d8c
Timothy J. Foley <timfoleysama@users.sourceforge.net>
2004-04-23 00:47:19 +0000

    Updated 4-way matrix multiply tests to run again...

--- prog/apps/timing/matmult4x4.h
+++ prog/apps/timing/matmult4x4.h
@@ -3,10 +3,11 @@
 #ifndef __MATMULT_4X4_H__
 #define __MATMULT_4X4_H__
 
+#include <brook.hpp>
+#include "main.h"
+
 extern void Matmult4x4_1way_Time( int inStreamSize );
-extern void Matmult4x4_1wayPretransposed_Time( int inStreamSize );
 extern void Matmult4x4_4way_Time( int inStreamSize );
-extern void Matmult4x4_4wayPretransposed_Time( int inStreamSize );
 
 #endif
 
