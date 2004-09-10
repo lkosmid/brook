23dc96d460a999a8d246191681282fb12e2de2b8
Ian Buck <ianbuck@users.sourceforge.net>
2004-09-10 03:49:51 +0000

    Forced loading of function pointers on GLX

--- runtime/gpu/ogl/oglfunc.cpp
+++ runtime/gpu/ogl/oglfunc.cpp
@@ -25,13 +25,26 @@ void brook::initglfunc(void) {
 #ifdef WIN32
 #define  XXX(type, fn) fn = (type) wglGetProcAddress(#fn); \
                        GPUAssert(fn, "Failed to load" #fn);
-  RUNTIME_BONUS_GL_FNS;
-  RUNTIME_BONUS_WGL_FNS;
-#undef XXX   
-#define  XXX(type, fn) fn = (type) wglGetProcAddress(#fn);
-  RUNTIME_BONUS_GL_FNS_ATI;
-#undef XXX   
+#else
+#define  XXX(type, fn) fn = (type) glXGetProcAddress(#fn); \
+                       GPUAssert(fn, "Failed to load" #fn);
 #endif
+
+  RUNTIME_BONUS_GL_FNS;
+
+#ifdef WIN32
+  RUNTIME_BONUS_WGL_FNS;
+#endif
+
+#undef XXX   
+#ifdef WIN32
+#define  XXX(type, fn) fn = (type) wglGetProcAddress(#fn);
+#else
+#define  XXX(type, fn) fn = (type) glXGetProcAddress(#fn);
+#endif
+
+  RUNTIME_BONUS_GL_FNS_ATI;
+
 }
 
 
