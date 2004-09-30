aedcda13533807ad62ea0b59f3b9e1f1e8b0d162
Daniel Reiter Horn <danielrh@users.sourceforge.net>
2004-09-30 06:46:31 +0000

    no more printing crap

--- prog/tests/fft/wfft.cpp
+++ prog/tests/fft/wfft.cpp
@@ -56,7 +56,7 @@ brook::stream &getW(int k, int logN, bool vertical) {
     ret = new brook::stream (flawt2,N/2,1,-1);
   }
   float2 *W = new float2 [N/2];
-  printf ("W logn:%d k:%d N:%d Stride:%d:\n",logN,k,N,Stride);
+  //printf ("W logn:%d k:%d N:%d Stride:%d:\n",logN,k,N,Stride);
   for (int i=0;i<N/2;++i) {
     int aindex=i*2;
     int bindex=BitReverse(aindex,logN);
@@ -64,7 +64,7 @@ brook::stream &getW(int k, int logN, bool vertical) {
     float ang=index*3.1415926536/(N/2);
     W[i].x=cos(ang);
     W[i].y=sin(ang);
-    printf ("index: %d i:%d b:%d a:(%f) %f %f] ",index,aindex,bindex,ang*180/3.1415926536,W[i].x,W[i].y);
+    //printf ("index: %d i:%d b:%d a:(%f) %f %f] ",index,aindex,bindex,ang*180/3.1415926536,W[i].x,W[i].y);
   }
   printf ("\n");
   streamRead(*ret,W);
