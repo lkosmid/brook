f75a2b5142f1ba75690f0154538ffd9baf3f2d94
Mike Houston <mikehouston@users.sourceforge.net>
2007-05-19 19:41:37 +0000

    Fixup for Vista compile with newer cygwin

--- config/DetectOS.mk
+++ config/DetectOS.mk
@@ -5,12 +5,15 @@ ARCH=$(shell uname | sed -e 's/-.*//g')
 ifeq ($(ARCH), CYGWIN)
 OS=Windows_NT
 else
+ifeq ($(ARCH), CYGWIN_NT)
+OS=Windows_NT
+else
 ifeq ($(ARCH), Linux)
 OS=Linux
 else
 OS=Default
 endif
 endif
-
+endif
 endif
 
