c9e6e1720cde171c1c800465ae93ac5bbd6e2118
Leonidas Kosmidis <lkosmid@users.noreply.github.com>
2015-12-28 21:10:10 +0000

    Preparing the building infrastructure to build on Raspberry Pi

--- config/LinuxGLES.mk
+++ config/LinuxGLES.mk
@@ -13,24 +13,24 @@ BINSUFFIX        :=
 
 SYSTEM_LIBS      := 
 
-CFLAGS           += -DBUILD_GLES
+CFLAGS           += -DBUILD_GLES -DRPI_NO_X
 #CFLAGS           += -fopenmp 
 C_CPP_FLAG	 := -E
 C_INCLUDE_FLAG   := -I
-C_DEBUG_FLAG     := -g3 -msse -Wall 
-C_RELEASE_FLAG   := -O3 -msse -funroll-all-loops  -fexpensive-optimizations -ffast-math -finline-functions -frerun-loop-opt 
+C_DEBUG_FLAG     := -g3 -Wall 
+C_RELEASE_FLAG   := -O3 -funroll-all-loops  -fexpensive-optimizations -ffast-math -finline-functions -frerun-loop-opt 
 C_STATIC_FLAG    := 
 C_OUTPUT_FLAG    := -o 
 C_COMPILE_FLAG   := -c 
 
-GLES_INCLUDE_DIR :=/usr/include/
-EGL_INCLUDE_DIR  :=/usr/include/
+GLES_INCLUDE_DIR :=/opt/vc/include
+EGL_INCLUDE_DIR  :=/opt/vc/include
 GLES_LD_DIR      :=/usr/lib/intel-cdv/
 EGL_LD_DIR       :=/usr/lib/intel-cdv/
 
-CFLAGS           += $(C_INCLUDE_FLAG)$(GLES_INCLUDE_DIR) $(C_INCLUDE_FLAG)$(EGL_INCLUDE_DIR)
+CFLAGS           += $(C_INCLUDE_FLAG)$(GLES_INCLUDE_DIR) $(C_INCLUDE_FLAG)$(EGL_INCLUDE_DIR) $(C_INCLUDE_FLAG)/opt/vc/include $(C_INCLUDE_FLAG)/opt/vc/include/interface/vcos/pthreads $(C_INCLUDE_FLAG)/opt/vc/include/interface/vmcs_host/linux
 
-LDFLAGS           += -lpthread -L/usr/X11R6/lib -L$(GLES_LD_DIR) -L$(EGL_LD_DIR)
+LDFLAGS           += -lpthread -L/usr/X11R6/lib -L$(GLES_LD_DIR) -L$(EGL_LD_DIR) -lGLESv2 -lEGL -lm -lbcm_host -L/opt/vc/lib
 #LDFLAGS           += -lgomp 
 LD_LIBDIR_FLAG    := -L
 LD_SHARED_FLAG    := -shared   
@@ -43,7 +43,7 @@ LIBRARIES        +=
 ARFLAGS		 := -rc 
 AR_OUTPUT_FLAG	 := 
 
-RUNTIME_LIBS     := brook gles cpu gpu X11 Xt GLESv2 EGL pthread
+RUNTIME_LIBS     := brook gles cpu gpu X11 GLESv2 EGL pthread
 
 RANLIB           := ranlib
 #Compile all benchmarks for gles and cpu backends
